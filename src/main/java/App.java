a05b12e593eec9aed06a9ef092b33bfc0e296015
marsulin <marsulin@mail.ru>
2023-12-06 13:04:53 +0300

    Создание конфигурации Spring-контейнера

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,9 +4,17 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
+            new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+            (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 =
+            (HelloWorld) applicationContext.getBean("helloworld");
+        Cat cat_bean = (Cat) applicationContext.getBean("cat");
+        Cat cat_bean2 = (Cat) applicationContext.getBean("cat");
+
         System.out.println(bean.getMessage());
+        System.out.println("hwbean eq: " + bean.equals(bean2));
+        System.out.println("catbean eq: " + cat_bean.equals(cat_bean2));
+
     }
 }
